83ed435cbe2022fea245be1d9ff4dedc8d160225
easy <easybrother0103@gmail.com>
2022-08-10 19:21:37 +0900

    init

---
+++ src/main/java/com/smokers/dampigo/DampigoApplication.java
@@ -0,0 +1,13 @@
+package com.smokers.dampigo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DampigoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DampigoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/smokers/dampigo/DampigoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smokers.dampigo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DampigoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
